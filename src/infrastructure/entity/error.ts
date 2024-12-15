a5f1cc7686ca050eda79e670e8df46864aee7494
Muhammad Ihsan <ihsan@femaledaily.com>
2024-12-15 17:27:26 +0700

    feat: add instace and error

--- src/infrastructure/entity/error.ts
+++ src/infrastructure/entity/error.ts
@@ -8,3 +8,25 @@ export class DBError extends Error {
 		this.code = "DB_ERROR";
 	}
 }
+
+export class AuthorizationError extends Error {
+	public status: number;
+	public code: "AUTHORIZATION_ERROR";
+
+	constructor(message: string) {
+		super(message);
+		this.status = 401;
+		this.code = "AUTHORIZATION_ERROR";
+	}
+}
+
+export class NotFoundError extends Error {
+	public status: number;
+	public code: "NOTFOUND_ERROR";
+
+	constructor(message: string) {
+		super(message);
+		this.status = 404;
+		this.code = "NOTFOUND_ERROR";
+	}
+}
--- src/infrastructure/entity/type.ts
+++ src/infrastructure/entity/type.ts
@@ -1,5 +1,6 @@
 export const TYPES = {
 	userRepo: Symbol.for("UserRepository"),
+	sessionRepo: Symbol.for("SessionRepository"),
 	prisma: Symbol.for("PrismaClient"),
 	logger: Symbol.for("Logger"),
 };
