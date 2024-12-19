a32d14707165276fb040a5f34a2e5e1e3999fe1c
Muhammad Ihsan <ihsan@femaledaily.com>
2024-12-19 10:17:59 +0700

    feat: register category repository and services in the dependency injection container

--- src/application/instance.ts
+++ src/application/instance.ts
@@ -2,8 +2,10 @@ import { Container } from "inversify";
 import { TYPES } from "../infrastructure/entity/type";
 import { UserRepository } from "../infrastructure/db/userRepo";
 import { SessionRepository } from "../infrastructure/db/sessionRepo";
+import { CategoryRepository } from "../infrastructure/db/categoryRepo";
 import { PrismaClient } from "@prisma/client";
 import { AuthServices } from "./services/authServices";
+import { CategoryServices } from "./services/categoryServices";
 
 const container = new Container();
 
@@ -11,8 +13,12 @@ container.bind(TYPES.prisma).toConstantValue(new PrismaClient());
 
 container.bind(TYPES.userRepo).to(UserRepository);
 container.bind(TYPES.sessionRepo).to(SessionRepository);
+container.bind(TYPES.categoryRepo).to(CategoryRepository);
 
 container.bind(AuthServices).toSelf();
+container.bind(CategoryServices).toSelf();
 
 // instances
 export const authServices = container.get<AuthServices>(AuthServices);
+export const categoryServices =
+	container.get<CategoryServices>(CategoryServices);
