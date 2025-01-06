170fc38b76b469651b7a068f3d080b8fc36098ad
Muhammad Ihsan <ihsan@femaledaily.com>
2025-01-06 23:16:19 +0700

    feat: implement ingredient management routes with API key validation and error handling

--- src/presentation/router/ingredientRouter.ts
+++ src/presentation/router/ingredientRouter.ts
@@ -1,8 +1,197 @@
 import Elysia, { t } from "elysia";
 import { authServices, ingredientService } from "../../application/instance";
-import { AuthorizationError } from "../../infrastructure/entity/error";
+import {
+	ApplicationError,
+	AuthorizationError,
+} from "../../infrastructure/entity/error";
+import { generalDTO } from "../../application/dtos/generalDTO";
 
 export const ingredientRouter = new Elysia({ prefix: "/v1" })
+
+	.guard(
+		{
+			headers: t.Object({
+				authorization: t.TemplateLiteral("Bearer ${string}"),
+				"api-key": t.String(),
+			}),
+
+			beforeHandle: async ({ headers }) => {
+				const sessionId = headers.authorization?.split(" ")[1];
+				if (!sessionId) throw new AuthorizationError("Unauthorized");
+
+				const { user } = await authServices.decodeSession(sessionId);
+				if (user.role !== "ADMIN")
+					throw new AuthorizationError("You are not allow");
+			},
+		},
+		(app) =>
+			app
+
+				// * Create new ingredient
+				.post(
+					"/ingredients",
+					async ({ body, set }) => {
+						try {
+							const newIngredient = await ingredientService.create({
+								name: body.name.toLocaleLowerCase(),
+								imageUrl: body.imageUrl ?? "",
+							});
+
+							set.status = 201;
+							return new generalDTO(
+								"success",
+								"create ingredient successfully",
+								201,
+								newIngredient,
+							).dataResult();
+						} catch (error) {
+							if (error instanceof ApplicationError) {
+								set.status = error.status;
+
+								return new generalDTO(
+									"error",
+									error.message,
+									set.status,
+									null,
+								).dataResult();
+							}
+
+							set.status = 500;
+							const errorMessage =
+								error instanceof Error
+									? error.message
+									: "Something went wrong!";
+
+							return new generalDTO(
+								"error",
+								errorMessage,
+								set.status,
+								null,
+							).dataResult();
+						}
+					},
+					{
+						detail: {
+							tags: ["Ingredients"],
+							description: "Create a new ingredient (Admin only).",
+						},
+
+						body: t.Object({
+							name: t.String(),
+							imageUrl: t.String(),
+						}),
+					},
+				)
+
+				// * Update ingredient
+				.patch(
+					"/ingredients/:ingredientId",
+					async ({ params, body, set }) => {
+						try {
+							const updatedIngredient = await ingredientService.update(
+								params.ingredientId,
+								{
+									name: body.name?.toLocaleLowerCase(),
+									imageUrl: body.imageUrl ?? "",
+								},
+							);
+
+							set.status = 200;
+							return new generalDTO(
+								"success",
+								"update ingredient successfully",
+								200,
+								updatedIngredient,
+							).dataResult();
+						} catch (error) {
+							if (error instanceof ApplicationError) {
+								set.status = error.status;
+
+								return new generalDTO(
+									"error",
+									error.message,
+									set.status,
+									null,
+								).dataResult();
+							}
+
+							set.status = 500;
+							const errorMessage =
+								error instanceof Error
+									? error.message
+									: "Something went wrong!";
+
+							return new generalDTO(
+								"error",
+								errorMessage,
+								set.status,
+								null,
+							).dataResult();
+						}
+					},
+					{
+						detail: {
+							tags: ["Ingredients"],
+							description: "Update an ingredient by ID (Admin only).",
+						},
+
+						body: t.Object({
+							name: t.String(),
+							imageUrl: t.String(),
+						}),
+					},
+				)
+
+				// * Delete ingredient
+				.delete(
+					"/ingredients/:ingredientId",
+					async ({ params, set }) => {
+						try {
+							await ingredientService.delete(params.ingredientId);
+
+							set.status = 200;
+							return new generalDTO(
+								"success",
+								"delete ingredient successfully",
+								200,
+								{
+									message: "Ingredient deleted",
+								},
+							).dataResult();
+						} catch (error) {
+							if (error instanceof ApplicationError) {
+								set.status = error.status;
+
+								return new generalDTO(
+									"error",
+									error.message,
+									set.status,
+									null,
+								).dataResult();
+							}
+
+							set.status = 500;
+							const errorMessage =
+								error instanceof Error
+									? error.message
+									: "Something went wrong!";
+
+							return new generalDTO(
+								"error",
+								errorMessage,
+								set.status,
+								null,
+							).dataResult();
+						}
+					},
+					{
+						detail: {
+							tags: ["Ingredients"],
+							description: "Delete an ingredient by ID (Admin only).",
+						},
+					},
+				),
+	)
 
 	// * Get all ingredients
 	.get(
@@ -11,15 +200,35 @@ export const ingredientRouter = new Elysia({ prefix: "/v1" })
 			try {
 				const allIngredients = await ingredientService.getAll();
 
-				return allIngredients;
+				set.status = 200;
+				return new generalDTO(
+					"success",
+					"get ingredients successfully",
+					200,
+					allIngredients,
+				).dataResult();
 			} catch (error) {
-				set.status = 500;
+				if (error instanceof ApplicationError) {
+					set.status = error.status;
 
-				if (error instanceof Error) {
-					throw new Error(error.message);
+					return new generalDTO(
+						"error",
+						error.message,
+						set.status,
+						null,
+					).dataResult();
 				}
 
-				throw new Error("Something went wrong!");
+				set.status = 500;
+				const errorMessage =
+					error instanceof Error ? error.message : "Something went wrong!";
+
+				return new generalDTO(
+					"error",
+					errorMessage,
+					set.status,
+					null,
+				).dataResult();
 			}
 		},
 		{
@@ -39,15 +248,35 @@ export const ingredientRouter = new Elysia({ prefix: "/v1" })
 					query.recipeId,
 				);
 
-				return ingredients;
+				set.status = 200;
+				return new generalDTO(
+					"success",
+					"get ingredients by recipe id successfully",
+					200,
+					ingredients,
+				).dataResult();
 			} catch (error) {
-				set.status = 500;
+				if (error instanceof ApplicationError) {
+					set.status = error.status;
 
-				if (error instanceof Error) {
-					throw new Error(error.message);
+					return new generalDTO(
+						"error",
+						error.message,
+						set.status,
+						null,
+					).dataResult();
 				}
 
-				throw new Error("Something went wrong!");
+				set.status = 500;
+				const errorMessage =
+					error instanceof Error ? error.message : "Something went wrong!";
+
+				return new generalDTO(
+					"error",
+					errorMessage,
+					set.status,
+					null,
+				).dataResult();
 			}
 		},
 		{
@@ -71,163 +300,41 @@ export const ingredientRouter = new Elysia({ prefix: "/v1" })
 					params.ingredientIdOrName,
 				);
 
-				return ingredient;
+				set.status = 200;
+				return new generalDTO(
+					"success",
+					"get ingredient successfully",
+					200,
+					ingredient,
+				).dataResult();
 			} catch (error) {
-				set.status = 500;
+				if (error instanceof ApplicationError) {
+					set.status = error.status;
 
-				if (error instanceof Error) {
-					throw new Error(error.message);
+					return new generalDTO(
+						"error",
+						error.message,
+						set.status,
+						null,
+					).dataResult();
 				}
 
-				throw new Error("Something went wrong!");
+				set.status = 500;
+				const errorMessage =
+					error instanceof Error ? error.message : "Something went wrong!";
+
+				return new generalDTO(
+					"error",
+					errorMessage,
+					set.status,
+					null,
+				).dataResult();
 			}
 		},
 		{
 			detail: {
 				tags: ["Ingredients"],
 				description: "Fetch a single ingredient by its ID or name.",
-			},
-		},
-	)
-
-	// * Create new ingredient
-	.post(
-		"/ingredients",
-		async ({ body, set }) => {
-			try {
-				const newIngredient = await ingredientService.create({
-					name: body.name.toLocaleLowerCase(),
-					imageUrl: body.imageUrl ?? "",
-				});
-
-				return newIngredient;
-			} catch (error) {
-				set.status = 500;
-
-				if (error instanceof Error) {
-					throw new Error(error.message);
-				}
-
-				throw new Error("Something went wrong!");
-			}
-		},
-		{
-			detail: {
-				tags: ["Ingredients"],
-				description: "Create a new ingredient (Admin only).",
-			},
-
-			headers: t.Object({
-				authorization: t.String({ description: "Bearer token" }),
-			}),
-
-			body: t.Object({
-				name: t.String(),
-				imageUrl: t.String(),
-			}),
-
-			// * Middleware
-			beforeHandle: async ({ headers }) => {
-				const sessionId = headers.authorization?.split(" ")[1];
-
-				if (!sessionId) throw new AuthorizationError("Unauthorized");
-
-				const { user } = await authServices.decodeSession(sessionId);
-
-				if (user.role !== "ADMIN") throw new AuthorizationError("Unauthorized");
-			},
-		},
-	)
-
-	// * Update ingredient
-	.patch(
-		"/ingredients/:ingredientId",
-		async ({ params, body, set }) => {
-			try {
-				const updatedIngredient = await ingredientService.update(
-					params.ingredientId,
-					{
-						name: body.name?.toLocaleLowerCase(),
-						imageUrl: body.imageUrl ?? "",
-					},
-				);
-
-				return updatedIngredient;
-			} catch (error) {
-				set.status = 500;
-
-				if (error instanceof Error) {
-					throw new Error(error.message);
-				}
-
-				throw new Error("Something went wrong!");
-			}
-		},
-		{
-			detail: {
-				tags: ["Ingredients"],
-				description: "Update an ingredient by ID (Admin only).",
-			},
-
-			headers: t.Object({
-				authorization: t.String({ description: "Bearer token" }),
-			}),
-
-			body: t.Object({
-				name: t.String(),
-				imageUrl: t.String(),
-			}),
-
-			// * Middleware
-			beforeHandle: async ({ headers }) => {
-				const sessionId = headers.authorization?.split(" ")[1];
-
-				if (!sessionId) throw new AuthorizationError("Unauthorized");
-
-				const { user } = await authServices.decodeSession(sessionId);
-
-				if (user.role !== "ADMIN") throw new AuthorizationError("Unauthorized");
-			},
-		},
-	)
-
-	// * Delete ingredient
-	.delete(
-		"/ingredients/:ingredientId",
-		async ({ params, set }) => {
-			try {
-				await ingredientService.delete(params.ingredientId);
-
-				return { message: "Ingredient deleted" };
-			} catch (error) {
-				set.status = 500;
-
-				if (error instanceof Error) {
-					throw new Error(error.message);
-				}
-
-				throw new Error("Something went wrong!");
-			}
-		},
-		{
-			detail: {
-				tags: ["Ingredients"],
-				description: "Delete an ingredient by ID (Admin only).",
-			},
-
-			headers: t.Object({
-				authorization: t.String({ description: "Bearer token" }),
-			}),
-
-			// * Middleware
-			beforeHandle: async ({ headers }) => {
-				const sessionId = headers.authorization?.split(" ")[1];
-
-				if (!sessionId) throw new AuthorizationError("Unauthorized");
-
-				const { user } = await authServices.decodeSession(sessionId);
-
-				if (user.role !== "ADMIN") throw new AuthorizationError("Unauthorized");
 			},
 		},
 	);
